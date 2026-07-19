a45e6da8802f256603156c84192b1acbfd53b966
Sowmya <sowmyavelugubantla5@gmail.com>
2026-07-19 11:47:51 +0530

    Day 2: Added input fields for name, subjecct, hours

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 st.title("Study Tracker")
-st.write("Hello, this is my first Streamlit app!")
+st.write("Track your daily study progress!")
+
+name = st.text_input("Enter your name:")
+subject = st.text_input("Enter subject studied today:")
+hours = st.number_input("Hours studied:", min_value=0.0, max_value=24.0, step=0.5)
+
+if st.button("Submit"):
+    st.success(f"Great job, {name}! You studied {subject} for {hours} hours today.")
